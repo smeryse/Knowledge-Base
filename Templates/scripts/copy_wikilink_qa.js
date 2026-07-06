302c0a15bb3f3e38930ac88ad6a866fe41ae797e
smeryse <luxiut@yandex.ru>
2026-07-06 17:02:57 +0300

    vault backup: 2026-07-06 17:02:57

--- Templates/scripts/copy_wikilink_qa.js
+++ Templates/scripts/copy_wikilink_qa.js
@@ -1,10 +1,11 @@
-module.exports = async () => {
-    const activeFile = app.workspace.getActiveFile();
+module.exports = async (params) => {
+    const activeFile = params.app.workspace.getActiveFile();
     if (!activeFile) {
         new Notice("Нет активного файла");
         return;
     }
     const wikilink = `[[${activeFile.basename}]]`;
-    await navigator.clipboard.writeText(wikilink);
+    const { clipboard } = window.require("electron");
+    clipboard.writeText(wikilink);
     new Notice(`Скопировано: ${wikilink}`);
 };
